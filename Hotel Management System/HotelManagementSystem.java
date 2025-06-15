c9788a9a28b3f213c1d8d0c1b65a504debc225c0
YeddulaSaiHarshitha <127549006+SaiHarshitha-1234@users.noreply.github.com>
2025-06-15 06:27:57 +0530

    Update HotelManagementSystem.java

--- Hotel Management System/HotelManagementSystem.java
+++ Hotel Management System/HotelManagementSystem.java
@@ -11,7 +11,7 @@ public class HotelManagementSystem {
 import java.sql.*;
 
 public class DBConnection {
-    public static Connection getConnection() {
+    public static synchronized Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_db", "root", "password");
